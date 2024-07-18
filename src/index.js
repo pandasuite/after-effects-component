8554fcb56e5ef597113b626860fa8e20b1c84f91
Benjamin Barbe <benjamin@pandasuite.com>
2024-07-18 16:54:58 +0200

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-mixed-operators */
-/* eslint-disable radix */
-/* eslint-disable no-use-before-define */
-import PandaBridge from 'pandasuite-bridge';
-import bodymovin from 'lottie-web';
-import './index.css';
+import PandaBridge from "pandasuite-bridge";
+import bodymovin from "lottie-web";
+import "./index.css";
 
 let properties = null;
 let markers = null;
@@ -20,24 +17,28 @@ PandaBridge.init(() => {
     properties = pandaData.properties;
     markers = pandaData.markers;
 
-    if (document.readyState === 'complete') {
+    if (document.readyState === "complete") {
       myInit();
     } else {
-      document.addEventListener('DOMContentLoaded', myInit, false);
+      document.addEventListener("DOMContentLoaded", myInit, false);
     }
   });
 
   function myInit() {
-    const jsonUrl = PandaBridge.resolvePath('animationFolder', './') + properties.animationPath;
-    // eslint-disable-next-line no-nested-ternary
-    const loop = properties.loop === -1
-      ? true
-      : (properties.loop === 0 ? false : properties.loop + 1);
+    const jsonUrl =
+      PandaBridge.resolvePath("animationFolder", "./") +
+      properties.animationPath;
+    const loop =
+      properties.loop === -1
+        ? true
+        : properties.loop === 0
+        ? false
+        : properties.loop + 1;
     nbLoop = properties.loop;
 
     animation = bodymovin.loadAnimation({
-      container: document.getElementById('container'),
-      renderer: PandaBridge.isStudio ? 'canvas' : 'svg',
+      container: document.getElementById("container"),
+      renderer: PandaBridge.isStudio ? "canvas" : "svg",
       loop,
       autoplay: false,
       path: jsonUrl,
@@ -45,27 +46,27 @@ PandaBridge.init(() => {
 
     window.animation = animation;
 
-    animation.addEventListener('data_ready', () => {
+    animation.addEventListener("data_ready", () => {
       /* Disable autoPlay in edition mode for convenience */
       if (properties.autoPlay && !PandaBridge.isStudio) {
         animation.play();
-        PandaBridge.send('onStartingPlay');
+        PandaBridge.send("onStartingPlay");
       }
 
       triggerUpdatedData(PandaBridge.INITIALIZED);
     });
 
-    animation.addEventListener('DOMLoaded', () => {
+    animation.addEventListener("DOMLoaded", () => {
       sendScreenshot();
     });
 
-    animation.addEventListener('enterFrame', onTimeUpdate);
-    animation.addEventListener('loopComplete', () => {
+    animation.addEventListener("enterFrame", onTimeUpdate);
+    animation.addEventListener("loopComplete", () => {
       if (nbLoop > 0) {
         nbLoop -= 1;
       }
     });
-    animation.addEventListener('complete', () => {
+    animation.addEventListener("complete", () => {
       /* BodyMovin use playCount for stopping the animation (refering to loop property) */
       /* We reset it to act like a new play */
       animation.playCount = -1;
@@ -75,7 +76,7 @@ PandaBridge.init(() => {
       fromSynchro = true;
       setFrame(0);
 
-      PandaBridge.send('onFinishPlaying');
+      PandaBridge.send("onFinishPlaying");
       triggerUpdatedData(PandaBridge.UPDATED, -1);
     });
 
@@ -114,66 +115,72 @@ PandaBridge.init(() => {
 
   /* Actions */
 
-  PandaBridge.listen('playPause', () => {
+  PandaBridge.listen("playPause", () => {
     if (animation && animation.isPaused) {
       animation.play();
-      PandaBridge.send('onStartingPlay');
+      PandaBridge.send("onStartingPlay");
     } else if (animation) {
       animation.pause();
-      PandaBridge.send('onPausePlaying');
+      PandaBridge.send("onPausePlaying");
     }
   });
 
-  PandaBridge.listen('play', () => {
+  PandaBridge.listen("play", () => {
     if (animation) {
       animation.play();
-      PandaBridge.send('onStartingPlay');
+      PandaBridge.send("onStartingPlay");
     }
   });
 
-  PandaBridge.listen('pause', () => {
+  PandaBridge.listen("pause", () => {
     if (animation && !animation.isPaused) {
       animation.pause();
-      PandaBridge.send('onPausePlaying');
+      PandaBridge.send("onPausePlaying");
     }
   });
 
-  PandaBridge.listen('stop', () => {
+  PandaBridge.listen("stop", () => {
     if (animation) {
       animation.stop();
       animation.playCount = 0;
       nbLoop = properties.loop;
-      PandaBridge.send('onStoppingPlay');
+      PandaBridge.send("onStoppingPlay");
       triggerUpdatedData(PandaBridge.UPDATED, -1);
     }
   });
 
-  PandaBridge.listen('seek', (args) => {
+  PandaBridge.listen("seek", (args) => {
     const props = args[0] || {};
 
-    setFrame(Math.min(Math.max(parseInt(props.frame), 0), animation.totalFrames - 1));
+    setFrame(
+      Math.min(Math.max(parseInt(props.frame), 0), animation.totalFrames - 1),
+    );
   });
 
-  PandaBridge.listen('forward', (args) => {
+  PandaBridge.listen("forward", (args) => {
     const props = args[0] || {};
 
-    setFrame(Math.min(animation.currentRawFrame + parseInt(props.frame),
-      animation.totalFrames - 1));
+    setFrame(
+      Math.min(
+        animation.currentRawFrame + parseInt(props.frame),
+        animation.totalFrames - 1,
+      ),
+    );
   });
 
-  PandaBridge.listen('rewind', (args) => {
+  PandaBridge.listen("rewind", (args) => {
     const props = args[0] || {};
 
     setFrame(Math.max(animation.currentRawFrame - parseInt(props.frame), 0));
   });
 
-  PandaBridge.listen('restartFromBeginning', () => {
+  PandaBridge.listen("restartFromBeginning", () => {
     if (animation) {
       animation.goToAndPlay(0);
     }
   });
 
-  PandaBridge.listen('setSpeed', (args) => {
+  PandaBridge.listen("setSpeed", (args) => {
     const props = args[0] || {};
 
     if (animation) {
@@ -181,46 +188,54 @@ PandaBridge.init(() => {
     }
   });
 
-  PandaBridge.listen('increaseSpeed', (args) => {
+  PandaBridge.listen("increaseSpeed", (args) => {
     const props = args[0] || {};
 
     if (animation) {
-      const speed = Math.min(currentSpeed + parseFloat(props.speed), properties.maxSpeed);
+      const speed = Math.min(
+        currentSpeed + parseFloat(props.speed),
+        properties.maxSpeed,
+      );
       animation.setSpeed(speed);
       currentSpeed = speed;
     }
   });
 
-  PandaBridge.listen('decreaseSpeed', (args) => {
+  PandaBridge.listen("decreaseSpeed", (args) => {
     const props = args[0] || {};
 
     if (animation) {
-      const speed = Math.max(currentSpeed - parseFloat(props.speed), properties.minSpeed);
+      const speed = Math.max(
+        currentSpeed - parseFloat(props.speed),
+        properties.minSpeed,
+      );
       animation.setSpeed(speed);
       currentSpeed = speed;
     }
   });
 
-  PandaBridge.synchronize('frame', (percent) => {
+  PandaBridge.synchronize("frame", (percent) => {
     if (animation) {
       fromSynchro = true;
 
       let additionalFrames = 0;
       if (properties.loop !== -1) {
-        nbLoop = properties.loop - Math.floor(percent / (100 / (properties.loop + 1)));
+        nbLoop =
+          properties.loop - Math.floor(percent / (100 / (properties.loop + 1)));
         additionalFrames = properties.loop * animation.totalFrames;
       }
       setFrame(
-        percent * (animation.totalFrames - 1 + additionalFrames) / 100
-        % animation.totalFrames,
+        ((percent * (animation.totalFrames - 1 + additionalFrames)) / 100) %
+          animation.totalFrames,
       );
     }
   });
 
-  PandaBridge.synchronize('speed', (percent) => {
+  PandaBridge.synchronize("speed", (percent) => {
     if (animation) {
       animation.setSpeed(
-        ((properties.maxSpeed - properties.minSpeed) * percent / 100) + properties.minSpeed,
+        ((properties.maxSpeed - properties.minSpeed) * percent) / 100 +
+          properties.minSpeed,
       );
     }
   });
@@ -248,21 +263,33 @@ PandaBridge.init(() => {
     /* Markers */
     if (markers) {
       markers.forEach((marker) => {
-        if (isValueInRange(marker.id, currentFrame, oldFrame, 0, animation.totalFrames - 1)) {
-          PandaBridge.send('triggerMarker', marker.id);
+        if (
+          isValueInRange(
+            marker.id,
+            currentFrame,
+            oldFrame,
+            0,
+            animation.totalFrames - 1,
+          )
+        ) {
+          PandaBridge.send("triggerMarker", marker.id);
         }
       });
     }
 
     /* Synchronisation status */
     if (!fromSynchro) {
-      let percent = parseInt(animation.currentRawFrame) * 100 / (animation.totalFrames - 1);
+      let percent =
+        (parseInt(animation.currentRawFrame) * 100) /
+        (animation.totalFrames - 1);
       if (properties.loop !== -1) {
-        percent = (parseInt(animation.currentRawFrame)
-        + ((properties.loop - nbLoop) * animation.totalFrames)) * 100
-          / (animation.totalFrames + (properties.loop * animation.totalFrames) - 1);
+        percent =
+          ((parseInt(animation.currentRawFrame) +
+            (properties.loop - nbLoop) * animation.totalFrames) *
+            100) /
+          (animation.totalFrames + properties.loop * animation.totalFrames - 1);
       }
-      PandaBridge.send('synchronize', [percent, 'frame', true]);
+      PandaBridge.send("synchronize", [percent, "frame", true]);
     } else {
       fromSynchro = false;
     }
@@ -270,17 +297,31 @@ PandaBridge.init(() => {
     oldFrame = currentFrame;
   }
 
-  function isValueInRange(testValue, currentValue, oldValue, minBorder, maxBorder) {
+  function isValueInRange(
+    testValue,
+    currentValue,
+    oldValue,
+    minBorder,
+    maxBorder,
+  ) {
     const minvalue = Math.min(oldValue, currentValue);
     const maxvalue = Math.max(oldValue, currentValue);
 
-    const isLooping = (Math.abs(oldValue - currentValue) > (maxBorder - minBorder) * 0.85)
-      && oldValue !== -1;
-    return ((isLooping && ((testValue > maxvalue && testValue <= maxBorder)
-        || (testValue >= minBorder && testValue <= minvalue)))
-      || (!isLooping
-        && ((oldValue <= currentValue && (testValue > minvalue && testValue <= maxvalue))
-        || (oldValue > currentValue && (testValue >= minvalue && testValue < maxvalue)))));
+    const isLooping =
+      Math.abs(oldValue - currentValue) > (maxBorder - minBorder) * 0.85 &&
+      oldValue !== -1;
+    return (
+      (isLooping &&
+        ((testValue > maxvalue && testValue <= maxBorder) ||
+          (testValue >= minBorder && testValue <= minvalue))) ||
+      (!isLooping &&
+        ((oldValue <= currentValue &&
+          testValue > minvalue &&
+          testValue <= maxvalue) ||
+          (oldValue > currentValue &&
+            testValue >= minvalue &&
+            testValue < maxvalue)))
+    );
   }
 
   function triggerUpdatedData(eventName, currentSeek) {
@@ -290,7 +331,7 @@ PandaBridge.init(() => {
       PandaBridge.send(eventName, {
         controls: [
           {
-            id: 'controlBar',
+            id: "controlBar",
             value: {
               minSeek: 0,
               maxSeek: animation.totalFrames,
@@ -304,11 +345,11 @@ PandaBridge.init(() => {
 
   function sendScreenshot() {
     if (PandaBridge.isStudio) {
-      const canvas = document.querySelector('canvas');
+      const canvas = document.querySelector("canvas");
 
       if (canvas) {
         PandaBridge.send(PandaBridge.SCREENSHOT_RESULT, [
-          canvas.toDataURL('image/png'),
+          canvas.toDataURL("image/png"),
         ]);
       }
     }
